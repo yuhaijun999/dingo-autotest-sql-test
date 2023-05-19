bde1b29ca87eea3ffb7c3607c7e087012059e521
Wentao274 <liwt@zetyun.com>
2023-05-19 16:23:47 +0800

    add DML test for MySQL protocol

--- src/test/java/listener/EmailableReporterListener.java
+++ src/test/java/listener/EmailableReporterListener.java
@@ -264,7 +264,7 @@ public class EmailableReporterListener implements IReporter{
         }
         
         writer.println("</table>");
-        writer.println("*注：Skipped表示本次测试被标记不需要执行的用例，并不是由于用例间依赖原因导致的跳过");
+        writer.println("*注：Skipped表示本次测试被标记不需要执行的用例或者由于数据库连接失败导致，并不是由于用例间依赖原因导致的跳过");
         writer.println("<br>");
         writer.println("<br>");
         String hrefUrl = "http://172.20.3.27/dingo/auto_tests/sql_report/" + CommonArgs.getCurDateStr("yyyyMMdd") + "/";
