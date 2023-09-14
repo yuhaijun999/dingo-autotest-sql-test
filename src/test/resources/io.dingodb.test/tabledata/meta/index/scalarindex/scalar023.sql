a6ba898c3b60450e9777e413430ddac3cbbc5637
Wentao274 <liwt@zetyun.com>
2023-09-14 23:30:17 +0800

    add scalar index with partition cases

---
+++ src/test/resources/io.dingodb.test/tabledata/meta/index/scalarindex/scalar023.sql
@@ -0,0 +1,19 @@
+CREATE TABLE $table (
+    id bigint,
+    name varchar(32),
+    age int,
+    gmt bigint,
+    price FLOAT,
+    amount DOUBLE,
+    address varchar(255),
+    birthday DATE,
+    create_time TIME,
+    update_time TIMESTAMP,
+    zip_code varchar(20),
+    is_delete boolean,
+    PRIMARY KEY (id),
+    index name_index (name),
+    index age_index (age),
+    index price_index (price) partition by range values (0.0),
+    index ct_index (create_time)
+)
--- src/test/resources/io.dingodb.test/tabledata/value/complexdatatype/multiset/value01_multiset1.sql
+++ src/test/resources/io.dingodb.test/tabledata/value/complexdatatype/multiset/value01_multiset1.sql
@@ -3,5 +3,5 @@ insert into $table values
 (2,'lisi',25,895.89,multiset[99]),
 (3,'wangwu',55,123.123,multiset[56781,-1]),
 (4,'LiuJi',100,0.0,multiset[4001,-4002,4003]),
-(5,'Kemi',-1,-1000.0,multiset[0,-2147483648,2147483647])
+(5,'Kemi',-1,-1000.0,multiset[0,-2147483648,2147483647]),
 (6,'Kemi',-1,-1000.0,multiset[12345,12345])
