94b3847def1ebca611634efd5481fcfa0e94d972
Wentao274 <liwt@zetyun.com>
2023-11-24 08:49:10 +0800

    add IVFFLAT cases

---
+++ src/test/resources/io.dingodb.test/tabledata/meta/index/vectorindex/flat/vector052.sql
@@ -0,0 +1,10 @@
+CREATE TABLE $table (
+    id bigint not null, 
+    name varchar(32),
+    age int,
+    amount double,
+    feature float array not null,
+    feature_id bigint not null,
+    primary key(id),
+    index feature_index vector(feature_id, feature) partition by hash partitions=5 parameters(type=flat, metricType=L2, dimension=64)
+)
---
+++ src/test/resources/io.dingodb.test/tabledata/meta/index/vectorindex/hnsw/vector050.sql
@@ -0,0 +1,10 @@
+CREATE TABLE $table (
+    id bigint not null, 
+    name varchar(32),
+    age int,
+    amount double,
+    feature float array not null,
+    feature_id bigint not null,
+    primary key(id),
+    index feature_index vector(feature_id, feature) partition by range values (100),(500),(1000) parameters(type=hnsw, metricType=L2, dimension=32, efConstruction=40, nlinks=32)
+)
