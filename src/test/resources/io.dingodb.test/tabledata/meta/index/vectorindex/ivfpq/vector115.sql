e6c49fc8640c3606f13dcdbb04439551c94b2fff
Wentao274 <liwt@zetyun.com>
2023-11-29 18:33:52 +0800

    add IVFPQ COSINE metricType cases

---
+++ src/test/resources/io.dingodb.test/tabledata/meta/index/vectorindex/ivfpq/vector115.sql
@@ -0,0 +1,10 @@
+CREATE TABLE $table (
+    id bigint auto_increment not null, 
+    name varchar(32),
+    age int,
+    amount double,
+    feature float array not null,
+    feature_id bigint not null,
+    index feature_index vector(feature_id, feature) parameters(type=ivfpq, metricType=COSINE, dimension=1536, ncentroids=24, nsubvector=64, nbits_per_idx=8),
+    primary key(id)
+)
