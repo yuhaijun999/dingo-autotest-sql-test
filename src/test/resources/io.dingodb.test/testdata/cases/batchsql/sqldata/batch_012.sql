4bdd1cf75285f1b22cb2d1e31fa8a4fe101abbda
Wentao274 <liwt@zetyun.com>
2023-12-27 19:54:51 +0800

    add one more case about auto_increment

---
+++ src/test/resources/io.dingodb.test/testdata/cases/batchsql/sqldata/batch_012.sql
@@ -0,0 +1,9 @@
+CREATE TABLE $table (
+    id int not null auto_increment,
+    name varchar(32),
+    age int,
+    PRIMARY KEY (id)
+);
+insert into $table values(-1,'zhangsan',18);
+insert into $table(name,age) values('lisi',22);
+select * from $table
